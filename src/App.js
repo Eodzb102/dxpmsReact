20cdaa05029a066e45c223adb6632cc63ef692b7
holymoly <you@example.commotessori.iron@montessori.co.kr>
2024-06-03 16:33:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './image/logo-montessori.png';
+import downarrow from './image/icon-stroke-arrow.svg';
+import './css/reset.css';
+import './css/style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="header_wrap">
+          <img src={logo} alt="한국몬테소리 로고" />
+          <div class="heaer_user_info">
+            <button>
+              몬테소리 님 <img src={downarrow} alt="더보기" />
+            </button>
+            <ul>
+              <li>
+                <a href="#!">정보수정</a>
+              </li>
+              <li>
+                <a href="#!">로그아웃</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </header>
     </div>
   );
